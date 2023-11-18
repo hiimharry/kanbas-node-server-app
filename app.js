f1f24063e07bb71fd90337ac78a1341d9c0a9015
Harry Allex <allexharry5@gmail.com>
2023-11-18 15:08:07 -0500

    a5

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 import express from "express";
-import Hello from "./hello.js";
+import "dotenv/config";
 import Lab5 from "./Lab5.js";
+import CourseRoutes from "./courses/routes.js";
+import ModuleRoutes from "./modules/routes.js";
 import cors from "cors";
 const app = express();
 app.use(cors());
 app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
 Lab5(app);
-Hello(app);
-app.listen(4000);
+app.listen(process.env.PORT || 4000);
